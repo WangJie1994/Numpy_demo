5e4a295600170741017b869baeb53f501cd0c3ef
Jie <wangjie19940416@icloud.com>
2018-05-15 07:14:46 +0800

    Initial commit

---
+++ numpy的属性.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+array = np.array([[1,2,3],
+                  [2,3,4]])
+
+
+print(array)
+print('dimension', array.ndim)
+print('shape', array.shape)
+print('size', array.size)
